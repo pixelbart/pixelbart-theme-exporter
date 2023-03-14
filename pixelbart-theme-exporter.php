4f9b98e4aaba0c5cd342ad232155aced72488c6a
Kevin Pliester <5339107+pixelbart@users.noreply.github.com>
2023-03-14 12:41:58 +0100

    1.0.0

---
+++ pixelbart-theme-exporter.php
@@ -0,0 +1,242 @@
+<?php
+/*
+Plugin Name: Pixelbart Theme Exporter
+Plugin URI: https://pixelbart.de
+Description: This plugin allows you to export the currently active WordPress theme as a ZIP file and optionally update the version number in its style.css file. It adds a tool to the WordPress admin area under Tools.
+Version: 1.0.0
+Tested up to: WordPress 6.1.1
+Requires at least: WordPress 4.0
+Author: Pixelbart
+Author URI: https://pixelbart.de
+License: MIT License
+License URI: https://opensource.org/licenses/MIT
+Text Domain: pixelbart-theme-exporter
+*/
+
+/**
+ * This function adds a new submenu page to the WordPress admin area under "Tools", using the
+ * `add_submenu_page()` function. The new submenu page is titled "Theme Exporter" and has a slug
+ * of "pxbt-theme-exporter". The page callback function is defined as `pxbt_exporter_adminpage()`,
+ * which generates the content for the page. The function is wrapped in an anonymous function to
+ * use as a callback for the `admin_menu` action hook.
+ *
+ * @return void
+ */
+add_action('admin_menu', function () {
+    $name = esc_html__('Theme Exporter', 'pixelbart-theme-exporter');
+    add_submenu_page('tools.php', $name, $name, 'manage_options', 'pxbt-theme-exporter', 'pxbt_exporter_adminpage');
+});
+
+
+/**
+ * Retrieve the version number of the active WordPress theme.
+ *
+ * This function uses the wp_get_theme() function to retrieve the active theme
+ * object, and then returns its version number. If the active theme version is not
+ * available, an empty string will be returned.
+ *
+ * @return string The version number of the active WordPress theme, or an empty string.
+ */
+function pxbt_exporter_theme_version()
+{
+    $theme = wp_get_theme();
+    return $theme->get('Version');
+}
+
+/**
+ * Increment the version number of a WordPress theme by 0.0.1, following the format major.minor.patch.build.
+ *
+ * @param string $version The current version number to increment.
+ *
+ * @return string The updated version number with the build number incremented by 1.
+ */
+function pxbt_exporter_next_theme_version($version)
+{
+    $parts = explode('.', $version);
+    $last_index = count($parts) - 1;
+    $parts[$last_index]++;
+
+    if ($parts[$last_index] == 10) {
+        $parts[$last_index] = 0;
+
+        if ($last_index > 0) {
+            $prev_index = $last_index - 1;
+            $parts[$prev_index]++;
+
+            for ($i = $prev_index + 1; $i < $last_index; $i++) {
+                if ($parts[$i] > 9) {
+                    $parts[$i] = 0;
+                }
+            }
+        }
+    }
+
+    $updated_version = implode('.', $parts);
+
+    if (count($parts) == 2) {
+        $updated_version .= '.0';
+    }
+
+    return $updated_version;
+}
+
+/**
+ * This function exports the currently active WordPress theme as a ZIP file and optionally updates
+ * the version number in its style.css file, based on user input. It first creates a temporary directory
+ * to copy the theme files to, applies the requested version number to the style.css file, compresses
+ * the files into a ZIP archive, and finally generates a download link to the user. The function also
+ * includes input validation and security checks, such as verifying a nonce and sanitizing user input.
+ */
+function pxbt_exporter_adminpage()
+{
+    $theme_dir = get_stylesheet_directory();
+    $theme_name = get_stylesheet();
+    $temp_theme_dir = plugin_dir_path(__FILE__) . 'temp_theme/' . $theme_name;
+    $temp_zip_dir = plugin_dir_path(__FILE__) . 'temp_zip/';
+
+    if (isset($_POST['_wpnonce']) && wp_verify_nonce($_POST['_wpnonce'], 'pixelbart-theme-exporter-nonce')) {
+        $theme_version = sanitize_text_field($_POST['version']);
+
+        if (!file_exists($temp_theme_dir)) {
+            mkdir($temp_theme_dir, 0755, true);
+        } elseif (!is_writable($temp_theme_dir)) {
+            chmod($temp_theme_dir, 0755);
+        }
+
+        pxbt_exporter_recursive_remove_directory($temp_theme_dir);
+        pxbt_exporter_recursive_copy($theme_dir, $temp_theme_dir);
+
+        $style_css = file_get_contents($temp_theme_dir . '/style.css');
+        $style_css = preg_replace('/(Version:\s*)[0-9]+(\.[0-9]+){0,3}/', '${1}' . $theme_version, $style_css);
+
+        file_put_contents($temp_theme_dir . '/style.css', $style_css);
+
+        if (!file_exists($temp_zip_dir)) {
+            mkdir($temp_zip_dir, 0755, true);
+        } elseif (!is_writable($temp_zip_dir)) {
+            chmod($temp_zip_dir, 0755);
+        }
+
+        $zip_filename = $temp_zip_dir . $theme_name . '.zip';
+
+        unlink($zip_filename);
+
+        $zip = new ZipArchive();
+        $zip->open($zip_filename, ZipArchive::CREATE | ZipArchive::OVERWRITE);
+
+        $files = new RecursiveIteratorIterator(
+            new RecursiveDirectoryIterator($temp_theme_dir),
+            RecursiveIteratorIterator::LEAVES_ONLY
+        );
+
+        foreach ($files as $name => $file) {
+            if (!$file->isDir()) {
+                $filePath = $file->getRealPath();
+                $relativePath = substr($filePath, strlen($temp_theme_dir) + 1);
+                $zip->addFile($filePath, $relativePath);
+            }
+        }
+
+        $zip->close();
+
+        $download_url = plugins_url('temp_zip/' . $theme_name . '.zip', __FILE__);
+        $message = __('The current WordPress theme has been exported as %s and can be downloaded here: <a href="%s" target="_blank">Download archiv</a>.', 'pixelbart-theme-exporter');
+        $message = sprintf($message, esc_html(basename($zip_filename)), esc_url($download_url));
+
+        echo '<div class="notice notice-success is-dismissible"><p>';
+        echo $message;
+        echo '</p></div>';
+    }
+       
+    pxbt_exporter_the_form();
+}
+
+/**
+ * This function generates the HTML form used to input the desired version number for the theme export,
+ * and displays the current theme version as well. It first retrieves the current version number using
+ * the `pxbt_exporter_theme_version()` function, and then generates the form fields, including a nonce
+ * field for security. The function also includes some HTML formatting and internationalization functions.
+ */
+function pxbt_exporter_the_form()
+{
+    $current_version = pxbt_exporter_theme_version();
+
+?>
+    <div class="wrap">
+        <h1><?php echo esc_html__('Pixelbart Theme Exporter', 'pixelbart-theme-exporter'); ?></h1>
+        <form method="post">
+            <?php wp_nonce_field('pixelbart-theme-exporter-nonce'); ?>
+            <p>
+                <label for="version"><?php echo esc_html__('New version:', 'pixelbart-theme-exporter'); ?></label>
+                <input type="text" name="version" id="version" value="<?= esc_html(pxbt_exporter_next_theme_version($current_version)) ?>" />
+            <div class="description"><?php printf(esc_html__('Current version: %s', 'pixelbart-theme-exporter'), $current_version); ?></div>
+            </p>
+            <p><input type="submit" value="<?php echo esc_html__('Export', 'pixelbart-theme-exporter'); ?>" class="button-primary" /></p>
+        </form>
+    </div>
+<?php
+}
+
+/**
+ * This function recursively copies a directory from the source path to the destination path,
+ * including all subdirectories and files. It uses PHP's built-in `opendir()` and `readdir()`
+ * functions to traverse the directory structure, and copies each file or folder individually
+ * using PHP's `copy()` function. If the destination folder does not exist, it is created using
+ * `mkdir()`. The function does not return a value, but modifies the file system.
+ *
+ * @param string $src The path to the source directory to copy from.
+ * @param string $dst The path to the destination directory to copy to.
+ * @return void
+ */
+function pxbt_exporter_recursive_copy($src, $dst)
+{
+    $dir = opendir($src);
+    @mkdir($dst);
+
+    while (($file = readdir($dir)) !== false) {
+        if ($file != '.' && $file != '..') {
+            if (is_dir($src . '/' . $file)) {
+                pxbt_exporter_recursive_copy($src . '/' . $file, $dst . '/' . $file);
+            } else {
+                copy($src . '/' . $file, $dst . '/' . $file);
+            }
+        }
+    }
+
+    closedir($dir);
+}
+
+/**
+ * This function recursively removes a directory and all its contents, including all subdirectories
+ * and files. It uses PHP's built-in `scandir()` function to list all files in the directory, and then
+ * iterates over each file, removing them recursively using either the `pxbt_exporter_recursive_remove_directory()`
+ * function (if the file is a directory), or the `unlink()` function (if the file is a regular file). If the
+ * `$exclusions` parameter is set, any files or directories in that array will be skipped during the removal
+ * process. The function returns `false` if the directory does not exist, or `true` if the directory was
+ * successfully removed.
+ *
+ * @param string $dir The path to the directory to remove.
+ * @param array $exclusions An optional array of files or directories to exclude from the removal process.
+ * @return bool Returns `true` if the directory was successfully removed, `false` otherwise.
+ */
+function pxbt_exporter_recursive_remove_directory($dir, $exclusions = [])
+{
+    if (!is_dir($dir)) {
+        return false;
+    }
+
+    // Remove any excluded files or directories from the list of files to be removed.
+    $files = array_diff(scandir($dir), ['.', '..'], $exclusions);
+
+    foreach ($files as $file) {
+        $path = $dir . '/' . $file;
+
+        if (is_dir($path)) {
+            pxbt_exporter_recursive_remove_directory($path, $exclusions);
+        } else {
+            unlink($path);
+        }
+    }
+
+    return rmdir($dir);
+}
---
+++ readme.md
@@ -0,0 +1,38 @@
+# Pixelbart Theme Exporter
+
+This is a WordPress plugin that allows you to export the currently active WordPress theme as a ZIP file and optionally update the version number in its `style.css` file. It adds a tool to the WordPress admin area under `Tools`.
+
+## Installation
+
+1. Upload the `pixelbart-theme-exporter` directory to the `/wp-content/plugins/` directory of your WordPress installation.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+
+## Usage
+
+1. Go to the 'Theme Exporter' tool under the 'Tools' menu in the WordPress admin area.
+2. Enter a new version number (optional) for the theme in the input field.
+3. Click the 'Export' button to export the theme as a ZIP file with the updated version number.
+
+## Security
+
+This plugin includes security measures such as input validation, data sanitization, and nonce verification to prevent unauthorized access or malicious code injection.
+
+## License
+
+This plugin is released under the MIT License. See the `LICENSE` file for details.
+
+## Support and Contributions
+
+If you encounter any issues or have any suggestions for improvement, please submit an issue or pull request on the GitHub repository. Contributions are welcome and appreciated.
+
+## Credits
+
+- Plugin author: Pixelbart
+- Plugin author URL: https://pixelbart.de/
+- Plugin version: 1.0.0
+- Tested up to: WordPress 6.1.1
+- Requires at least: WordPress 4.0
+
+## Changelog
+
+- 1.0.0 Initial release.
